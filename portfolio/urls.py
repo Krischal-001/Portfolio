763079af2853e199ae0811cac1a293f0622dd1e6
Krischal-001 <your_email@example.comkrischallamichhane001@gmail.com>
2025-11-07 22:53:45 +0545

    bootstrap integrated with django sucessfully

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   
 ]
